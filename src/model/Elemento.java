b80c0bd9ce3ddc0333a9ca2bc1706eea2010a55f
leandro <leandro.piccini@acad.pucrs.br>
2019-11-13 20:33:48 -0300

    Atualizando

--- src/model/Elemento.java
+++ src/model/Elemento.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
 
-/**
- *
- * @author 20416529
- */
+
 public enum Elemento {
-    FOGO, AGUA, TERRA
+    FOGO, AGUA, TERRA,RAIO
 }
