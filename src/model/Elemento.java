ae84f0097d4eaa849b57456a10a0f1cd70c972e4
Jonatasvangroll <jonatas.vangroll@gmail.com>
2019-11-19 21:50:35 -0300

    Update Elemento.java
    alteração da classe para tipo de elemento

--- src/model/Elemento.java
+++ src/model/Elemento.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package model;
+package pokemoncard;
 
-/**
- *
- * @author 20416529
- */
-public enum Elemento {
-    FOGO, AGUA, TERRA
+
+    public enum TipoElemento {
+    FOGO, AGUA, TERRA,RAIO,NORMAL
 }
