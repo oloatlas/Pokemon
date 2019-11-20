9848363483b00339db4e8c8e2da969f64794c036
Jonatasvangroll <jonatas.vangroll@gmail.com>
2019-11-20 10:01:12 -0300

    Add files via upload
    classe jogo

---
+++ src/Jogo.java
@@ -0,0 +1,20 @@
+
+package pokemoncard;
+
+
+    public class Jogo {
+      private Jogador jogador;
+
+    public Jogo(Jogador jogador) {
+        this.jogador = jogador;
+    }
+
+    public Jogador getJogador() {
+        return jogador;
+    }
+
+    public void setJogador(Jogador jogador) {
+        this.jogador = jogador;
+      }
+  
+    }
